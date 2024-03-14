451f0dca195639fd101bbcb85ba9324217941f58
Catriona Beamish <catriona.beamish@gmail.com>
2024-03-14 16:43:04 +0000

    initiate_model lacked directory info

--- api/load_checkpoints.py
+++ api/load_checkpoints.py
@@ -1,9 +1,9 @@
-from initiate_model import initiate_model
+from api.initiate_model import initiate_model
 
 def load_checkpoints():
     model=initiate_model()
 
-    checkpoint_dir = 'model_mathilda_2000_12mar'
+    checkpoint_dir = 'final_model'
 
     epoch_number = 100  # for example, to load from checkpoint_epoch-06
     checkpoint_path = f"{checkpoint_dir}/checkpoint_epoch-{epoch_number:02d}"
