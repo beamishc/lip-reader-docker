3b1802711bad5e920046a27885382a85f6fc04c7
Catriona Beamish <catriona.beamish@gmail.com>
2024-03-14 14:38:57 +0000

    write to npz if exists

--- api/fast.py
+++ api/fast.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 import numpy as np
 import json
-from tempfile import TemporaryFile
+import os
 
 app = FastAPI()
 
@@ -28,10 +28,16 @@ async def frames_to_model(idx: str, test: Request):
     frames = np.array(json.loads(data))
     all_frames = [np.array(frame)for frame in frames]
     print(all_frames.shape)
-    np.savez(f'frames_{idx}.npz', np.array(all_frames))
+
+    filename = 'frames.npz'
+
+    if os.path.isfile(filename):
+        with np.load(filename) as loaded_npz:
+            loaded_npz['results'] = np.array(loaded_npz['results'].tolist() + all_frames)
+    else:
+        np.savez(filename, results = np.array(all_frames))
 
     return {"message": "Received data for prediction"
-            , "npz_name": f'frames_{idx}.npz'
             , "prediction": "Hello I am mother (TEST DEFAULT)"}
 
 @app.get("/predict/")
