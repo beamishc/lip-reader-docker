296f95e8567f3531244b50cb3523aa9979b46d66
Catriona Beamish <catriona.beamish@gmail.com>
2024-03-14 20:55:01 +0000

    pointing at checkpoints

--- api/load_checkpoints.py
+++ api/load_checkpoints.py
@@ -3,10 +3,10 @@ from api.initiate_model import initiate_model
 def load_checkpoints():
     model=initiate_model()
 
-    checkpoint_dir = 'final_model'
+    # checkpoint_dir = 'api/final_model'
 
     epoch_number = 100  # for example, to load from checkpoint_epoch-06
-    checkpoint_path = f"{checkpoint_dir}/checkpoint_epoch-{epoch_number:02d}"
+    checkpoint_path = f"api/final_model/checkpoint_epoch-{epoch_number:02d}"
 
     model.load_weights(checkpoint_path)
 
