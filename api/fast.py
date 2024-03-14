4f5a6a22363c95d3a3ccff453cf011f9fead939e
Catriona Beamish <catriona.beamish@gmail.com>
2024-03-14 13:11:17 +0000

    adding variable grab

--- api/fast.py
+++ api/fast.py
@@ -15,6 +15,8 @@ app.add_middleware(
     allow_headers=['*'],
 )
 
+full_frames = []
+
 @app.get("/home/")
 def home():
     return {'message': "Hi"}
@@ -25,11 +27,16 @@ async def frames_to_model(test: Request):
     print('successful data conversion')
 
     frames = np.array(json.loads(data))
-    all_frames = np.array([np.array(frame)for frame in frames])
+    all_frames = [np.array(frame)for frame in frames]
     print(all_frames.shape)
+
+    full_frames = full_frames + all_frames
+
     return {"message": "Received data for prediction"
+            , "full_frames": f"{str(len(full_frames))}"
             , "prediction": "Hello I am mother (TEST DEFAULT)"}
 
 @app.get("/predict/")
 def prediction():
+    # result = model.predict(np.array(full_frames))
     return {"prediction": "Hello I am mother (TEST DEFAULT)"}
