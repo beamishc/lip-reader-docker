8351386891cc6e27031f780ef29e666ab05f0c58
Catriona Beamish <catriona.beamish@gmail.com>
2024-03-14 13:31:29 +0000

    adding npz save

--- api/fast.py
+++ api/fast.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 import numpy as np
 import json
+from tempfile import TemporaryFile
 
 app = FastAPI()
 
@@ -15,25 +16,22 @@ app.add_middleware(
     allow_headers=['*'],
 )
 
-full_frames = []
-
 @app.get("/home/")
 def home():
     return {'message': "Hi"}
 
 @app.post("/send_frames/")
-async def frames_to_model(test: Request):
+async def frames_to_model(idx: str, test: Request):
     data = await test.json()
     print('successful data conversion')
 
     frames = np.array(json.loads(data))
     all_frames = [np.array(frame)for frame in frames]
     print(all_frames.shape)
-
-    full_frames = full_frames + all_frames
+    np.savez(f'frames_{idx}.npz', np.array(all_frames))
 
     return {"message": "Received data for prediction"
-            , "full_frames": f"{str(len(full_frames))}"
+            , "npz_name": f'frames_{idx}.npz'
             , "prediction": "Hello I am mother (TEST DEFAULT)"}
 
 @app.get("/predict/")
